f4bc5ee3fc2a8ce6dde686dca5ac861a68d74e1c
EmmanuelArce12 <rubenemanuel.arce@estudiantes.unahur.edu.ar>
2025-11-26 14:34:52 -0300

    "creación de todas las tablas"

--- app.py
+++ app.py
@@ -414,4 +414,6 @@ def admin_status_all():
     return jsonify(data)
 
 if __name__ == '__main__': 
+    with app.app_context():
+        db.create_all() # <--- ¡ESTO ARREGLA EL ERROR 500 AL CONFIGURAR VOX!
     app.run(host='0.0.0.0', port=10000)
