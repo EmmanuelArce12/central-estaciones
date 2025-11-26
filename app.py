558a77353f945edca24871fdcc6da878dea0c21a
EmmanuelArce12 <130231183+EmmanuelArce12@users.noreply.github.com>
2025-11-26 09:09:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import random
 import string
 from flask import Flask, request, jsonify, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timedelta
