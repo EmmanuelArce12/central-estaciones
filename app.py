a7e99972324a708e2751471fb1368b0471e6c9f0
Emmaxx09 <rubenemanuel.arce@estudiantes.unahur.edu.ar>
2025-12-26 11:31:03 -0300

    "cambios"

--- app.py
+++ app.py
@@ -183,10 +183,14 @@ class User(UserMixin, db.Model):
     plain_password = db.Column(db.String(100))
     is_superadmin = db.Column(db.Boolean, default=False)
 
+    
     def set_password(self, password):
-        self.plain_password = password # Guardamos copia visible (opcional)
-        self.password_hash = generate_password_hash(password)
+        self.password_hash = generate_password_hash(
+            password,
+            method="pbkdf2:sha256"
+        )
 
+    
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
